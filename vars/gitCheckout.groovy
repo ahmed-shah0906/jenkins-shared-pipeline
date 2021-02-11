e748e2309071d126dd0fad48cef1fe8103b8edf3
Shahrukh Ahmed <shahrukh.ahmed@nagarro.com>
2021-02-11 14:53:47 +0530

    added another method

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -6,3 +6,27 @@ def call(Map stageParams) {
         userRemoteConfigs: [[ url: stageParams.url ]]
     ])
   }
+
+
+def List<PackageSource> getNugetSources(){
+    List<PackageSource> nugetPackageSources =[ new PackageSource(key: "Nuget_Feed", sourceUrl: "https://api.nuget.org/v3/index.json"),
+	                                           new PackageSource(
+																	key: "Source_Prod",
+																	sourceUrl: "https://abc.io/prod",
+																	protocolVersion: 2,
+																	credentialsId: "1"
+																)
+											 ]
+
+	        												
+	if(!BRANCH_NAME.equals('master')){
+      nugetPackageSources.add(new PackageSource(
+												  key: "Source_Dev",
+												  sourceUrl: "https://abc.io/dev",
+												  protocolVersion: 2,
+												  credentialsId: "2"
+											   )
+					     )
+	}	
+	return nugetPackageSources
+}
